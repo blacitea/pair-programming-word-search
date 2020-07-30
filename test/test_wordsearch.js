95a7bd706b8fbfa0330517f75fa6f12f2d60f6be
Angel Tsang <blacitea@gmail.com>
2020-07-30 03:09:44 +0000

    solved vertical & horizontal

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,10 +1,10 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the horizontal word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the horizontal word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +35,67 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return true if the vertical word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY');
+
+    assert.isTrue(result);
+  });
+  it("should return false if the vertical word is not present", function() {
+    const result = wordSearch([
+      ['A','W','C','F','Q','U','A','L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+    
+    assert.isFalse(result);
+  });
+  // it("should return true if the diagonally word is present", function() {
+  //   const result = wordSearch([
+  //     ['F','W','C','F','Q','U','A','L'],
+  //     ['S','R','I','N','F','E','L','D'],
+  //     ['Y','F','A','F','Q','U','A','L'],
+  //     ['H','M','J','N','E','V','R','G'],
+  //     ['W','H','C','S','K','E','R','L'],
+  //     ['B','F','R','E','N','E','Y','B'],
+  //     ['U','B','T','W','A','P','A','I'],
+  //     ['O','D','C','A','K','U','A','S'],
+  //     ['E','Z','K','F','Q','U','A','L'],
+  //   ], 'FRANK');
+  
+  //   assert.isTrue(result);
+  // });
+  // it("should return false if the diagonally word is not present", function() {
+  //   const result = wordSearch([
+  //     ['F','W','C','F','Q','U','A','L'], (0,0) F 
+  //     ['S','R','I','N','F','E','L','D'], (1,1) R
+  //     ['Y','F','A','F','Q','U','A','L'], (2,2) A
+  //     ['H','M','J','N','E','V','R','G'], (3,3) N
+  //     ['W','H','C','S','K','E','R','L'], (4,4) K
+  //     ['B','F','R','E','N','E','Y','B'],
+  //     ['U','B','T','W','A','P','A','I'],
+  //     ['O','D','C','A','K','U','A','S'],
+  //     ['E','Z','K','F','Q','U','A','L'],
+  //   ], 'TRAVIS');
+    
+  //   assert.isFalse(result);
+  // });
 });
+
+
+if word[0] is found in x,y... word[++] is in x++,y++
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,68 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  //   console.log(verticalJoin);
+  // console.log(horizontalJoin)
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let k of verticalJoin) {
+    if (k.includes(word)) return true;
+  }
+    
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+const transpose = (letters) => {
+  const rows = letters.length;
+  const cols = letters[0].length;
+  const grid = [];
+    
+  for (let j = 0; j < cols; j++) {
+    grid[j] = Array(rows);
+  }
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      grid[j][i] = letters[i][j];
+    }
+  }
+  return grid;
+};
+/*
+Given a matrix (2D array)
+Given a word (string)
+Find the word in the matrix (horizontally / vertically)
+get all the char of a row, check if match with word // loop row
+get all the char of a col, check if match with word // loop col
+if found, return true
+if not found, return false
+*/
+
+
+// For this challenge, you'll be implementing a word search solver, as a function that receives a 2D array of letters and a word. The function must:
+
+// Check to find the word horizontally and vertically
+// Return true if the word is found, and return false if the word is not found
+// NOTE: You do not need to check to see if a word is written backwards or diagonally.
+
+// This challenge comes with some initial (buggy!) code. We suggest approaching this problem with a TDD mindset, meaning "write tests, make the tests pass, repeat!".
+
+// There are already some tests in ./test/test_wordsearch.js, and you can run the tests with npm test. You'll find that one of the tests doesn't pass yet, so you'll want to start by editing the code in wordsearch.js to allow the tests to pass.
+
+// When the present tests are successful, ask yourself, "Do the current tests cover all the possible cases?" What if the word is written vertically, not horizontally? What about the case where the word matrix is an empty array? You'll have to write tests for these cases (and any others that you think of), and you might also have to modify the wordSearch function to make those new tests pass.
+
+//     let diaArray = [];
+//     let wordLength = word.length;
+//   let splitWord = word.split('');
+//   for (let a = 0; a < letters.length; a++) {
+//     let diaWord = "";
+//     for (let b = 0; b < letters[a].length; b++) {
+//         if (splitWord[a] === letters[a][b]) {
+//           // need some code here to make sure only diag is being compaired?
+//         return true;
+//       }
+//     }
+//     // console.log(diaWord);
+//   }
